f830823c6a211e7ce0ab2a40fbcca999482c8bde
x950450 <68738260+x950450@users.noreply.github.com>
2022-05-16 08:26:13 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,8 +1,8 @@
 provider "azurerm" {
   features {}
 
-  subscription_id = "${ secrets.subscription_id }"
-  client_id       = "${ secrets.client_id }"
-  client_secret   = "${ secrets.client_secret }"
-  tenant_id       = "${ secrets.tenant_id }"
+  subscription_id = var.subscription_id
+  client_id       = var.client_id
+  client_secret   = var.client_secret
+  tenant_id       = var.tenant_id
 }
