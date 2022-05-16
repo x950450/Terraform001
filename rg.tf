9c4bfbf40d204efd8ccaf2ead2a3a6a825f361e9
x950450 <68738260+x950450@users.noreply.github.com>
2022-05-16 07:16:07 +0530

    Update rg.tf

--- rg.tf
+++ rg.tf
@@ -1,14 +1,14 @@
 resource "azurerm_resource_group" "RG-Group-01" {
-  name     = "RG-GROUP-01"
-  location = "eastus"
+  name     = var.rg1-name
+  location = var.location
 }
 
 resource "azurerm_storage_account" "storageacc463365785748" {
   name                     = "storageacc463365785748"
   resource_group_name      = "${azurerm_resource_group.RG-Group-01.name}"
   location                 = "${azurerm_resource_group.RG-Group-01.location}"
-  account_tier             = "Standard"
-  account_replication_type = "LRS"
+  account_tier             = var.accounttier
+  account_replication_type = var.replicationtype
 
   tags = {
     environment = "staging"
