dad5c62af0809a91dfe82edad46ad05fc4265c07
x950450 <68738260+x950450@users.noreply.github.com>
2022-05-16 07:13:43 +0530

    Update rg.tf

--- rg.tf
+++ rg.tf
@@ -7,7 +7,7 @@ resource "azurerm_storage_account" "storageacc463365785748" {
   name                     = "storageacc463365785748"
   resource_group_name      = "${azurerm_resource_group.RG-Group-01.name}"
   location                 = "${azurerm_resource_group.RG-Group-01.location}"
-  #account_tier             = "Standard"
+  account_tier             = "Standard"
   account_replication_type = "LRS"
 
   tags = {
