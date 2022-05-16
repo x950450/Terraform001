de6e978c71b8019262f3dec5eed151ac2f192d1d
x950450 <68738260+x950450@users.noreply.github.com>
2022-05-16 07:39:20 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,8 +1,8 @@
 provider "azurerm" {
   features {}
 
-  subscription_id = "3e958f5d-ca02-4a7c-802a-df9ca714b90b"
-  client_id       = "8518bcd8-0187-46f2-8388-65b6f38bd85b"
-  client_secret   = "~Ij8Q~cL1bV.P_.34Fz4H3ZKcAScsZa_KVVkcdsu"
-  tenant_id       = "e7f6afcc-7dd9-4bf7-991d-c8c64fcc1bb0"
+  subscription_id = "${env.subscription_id}"
+  client_id       = "${env.client_id}"
+  client_secret   = "${env.client_secret}"
+  tenant_id       = "${env.tenant_id}"
 }
